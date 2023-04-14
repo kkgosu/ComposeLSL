ca6bcfec417dfa48f9cf1f980e6368b72701f0df
kkgosu <kostya96.08@mail.ru>
2023-04-14 18:49:15 +0300

    SubcomposeColumn.kt

---
+++ app/src/main/java/com/example/composelsl/components/SubcomposeColumn.kt
@@ -0,0 +1,72 @@
+package com.example.composelsl.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.Measurable
+import androidx.compose.ui.layout.Placeable
+import androidx.compose.ui.layout.SubcomposeLayout
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Constraints
+import androidx.compose.ui.unit.IntSize
+import androidx.compose.ui.unit.dp
+
+/**
+ * Column, который приводит ширину детей к размеру наиболее широкого из них
+ *
+ * @author Konstantin Koval on 14.04.2023
+ */
+@Composable
+fun SubcomposeColumn(
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit,
+) {
+    SubcomposeLayout(modifier = modifier) { constraints ->
+        var subcomposeIndex = 0
+        var placeables: List<Placeable> = subcompose(subcomposeIndex++, content).map {
+            it.measure(constraints)
+        }
+        val columnSize = placeables.fold(IntSize.Zero) { currentMax: IntSize, placeable: Placeable ->
+            IntSize(
+                width = maxOf(currentMax.width, placeable.width),
+                height = currentMax.height + placeable.height
+            )
+        }
+
+        // Remeasure every element using width of longest item using it as min width for
+        // every composable
+        if (placeables.isNotEmpty() && placeables.size > 1) {
+            placeables = subcompose(subcomposeIndex, content).map { measurable: Measurable ->
+                measurable.measure(Constraints(columnSize.width, constraints.maxWidth))
+            }
+        }
+
+        layout(columnSize.width, columnSize.height) {
+            var yPos = 0
+            placeables.forEach { placeable: Placeable ->
+                placeable.placeRelative(0, yPos)
+                yPos += placeable.height
+            }
+
+        }
+    }
+}
+
+@Preview
+@Composable
+fun SubcomposeColumnPreview() {
+    Column(verticalArrangement = Arrangement.spacedBy(8.dp)) {
+        SubcomposeColumn {
+            Text(text = "smol", modifier = Modifier.background(Color.LightGray))
+            Text(text = "Lorem Ipsum is simply dummy text", modifier = Modifier.background(Color.Blue))
+        }
+        SubcomposeColumn {
+            Text(text = "smol", modifier = Modifier.background(Color.LightGray))
+            Text(text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", modifier = Modifier.background(Color.Blue))
+        }
+    }
+}
